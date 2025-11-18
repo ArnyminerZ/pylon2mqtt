aa55b5738427f90295e4e74088e8e37145c1f0b0
Arnau Mora <arnyminerz@proton.me>
2025-11-18 09:26:21 +0100

    Update MQTT client initialization with API version

--- pylon_to_mqtt.py
+++ pylon_to_mqtt.py
@@ -306,7 +306,7 @@ def run(argv):
     #setup the MQTT Client for publishing and subscribing
     clientId = argumentValues['mqttUser'] + "_mqttclient_" + str(randint(100, 999))
     log.info("Connecting with clientId=" + clientId)
-    mqttClient = mqttclient.Client(clientId) 
+    mqttClient = mqttclient.Client(mqtt.CallbackAPIVersion.VERSION1, clientId) 
     mqttClient.username_pw_set(argumentValues['mqttUser'], password=argumentValues['mqttPassword'])
     mqttClient.on_connect = on_connect    
     mqttClient.on_disconnect = on_disconnect  
